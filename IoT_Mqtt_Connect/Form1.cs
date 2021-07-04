c38797144308333047cc9fe4a1b213842c2cb72e
Ngoc Phong <ngocphong260899@gmail.com>
2021-07-04 14:51:42 +0700

    firt commit

---
+++ IoT_Mqtt_Connect/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace IoT_Mqtt_Connect
+{
+    public partial class Form1 : Form
+    {
+         MqttClient client;
+         string data_recv;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new MqttClient("mqtt.ngoinhaiot.com", 1111, false, null, null, MqttSslProtocols.None);
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId, "ngocphong260899", "ngocphong260899");
+
+            client.Subscribe(new string[] { "ngocphong260899" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            /*
+            string relay1_on = "{\"status\":\"onnk\"}";
+            client.Publish("ngocphong260899", Encoding.UTF8.GetBytes(relay1_on), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            */
+        }
+
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+
+            var msgs = Encoding.UTF8.GetString(e.Message);
+            data_recv = msgs.ToString();
+            Console.WriteLine("this is data recv : " + data_recv);
+            //string relay1_on = "{\"status\":1}";
+            try
+            {
+                parseJson str_json = JsonConvert.DeserializeObject<parseJson>(data_recv);
+                if (str_json == null)
+                {
+                    Console.WriteLine("-***- Json not DeserializeObject -***-");
+                    return;
+                }
+
+                string stt = str_json.status;
+                string pos = str_json.pos;
+
+
+                button_get_state(button2, stt);
+
+
+
+
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Phong hg 56: Json not convert");
+            }
+            
+
+
+        }
+        public void button_get_state( Button btn, string stt)
+        {
+            btn.Invoke((MethodInvoker)(() => btn.Text = stt));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            MqttClient client = new MqttClient("mqtt.ngoinhaiot.com", 1111, false, null, null, MqttSslProtocols.None);
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId,"ngocphong260899","ngocphong260899");
+            */
+            string strValue = Convert.ToString(textBox1.Text);
+            client.Publish("ngocphong260899", Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+
+
+        }
+
+
+    }
+}
---
+++ IoT_Mqtt_Connect/parseJson.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace IoT_Mqtt_Connect
+{
+    class parseJson
+    {
+        [JsonProperty("status")]
+        public string status { get; set; }
+
+        [JsonProperty("sw_wifi")]
+        public string sw_wifi { get; set; }
+
+        [JsonProperty("pos")]
+        public string pos { get; set; }
+
+        [JsonProperty("control")]
+        public string control { get; set; }
+
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        [JsonProperty("ssid")]
+        public string ssid { get; set; }
+
+        [JsonProperty("password")]
+        public string password { get; set; }
+
+        [JsonProperty("streng")]
+        public string streng { get; set; }
+
+
+
+
+
+
+
+    }
+}
